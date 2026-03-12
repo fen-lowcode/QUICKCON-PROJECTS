f6179fd27e04ec59c62ed25f8b50cb7c66b6a4f7
fen-lowcode <223019650+fen-lowcode@users.noreply.github.com>
2026-03-12 13:43:17 +0800

    changed makefile to make it faster

--- Quicksearch/Quicksearch/prototype-v1/src/adminpanel.cpp
+++ Quicksearch/Quicksearch/prototype-v1/src/adminpanel.cpp
@@ -1,15 +1,27 @@
 #include "adminpanel.hpp"
 
 
-#define WIDTH 1300
-#define HEIGHT 800
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
 
 AdminPanel::AdminPanel(const wxString& title) : wxFrame (
 nullptr,
     wxID_ANY,
     title,
-    wxDefaultPosition,
-    wxSize(WIDTH, HEIGHT),
-    wxDEFAULT_FRAME_STYLE
-) {
+    wxDefaultPosition, wxSize(WINDOW_WIDTH, WINDOW_HEIGHT), wxCAPTION | wxSYSTEM_MENU | wxCLOSE_BOX | wxMINIMIZE_BOX)
+{
+    this -> Center();
+    
+    wxListCtrl* resultList = new wxListCtrl( panel,
+        wxID_ANY,
+        wxDefaultPosition,
+        wxDefaultSize,
+        wxLC_REPORT | wxLC_SINGLE_SEL | wxBORDER_SIMPLE 
+    );
+
+    resultList -> InsertColumn(0,  "ID", wxLIST_FORMAT_LEFT, 10);
+    resultList -> InsertColumn(1,  "Firstname", wxLIST_FORMAT_LEFT, 100);
+    resultList -> InsertColumn(2,  "Lastname", wxLIST_FORMAT_LEFT, 100);
+    resultList -> InsertColumn(3,  "Admin", wxLIST_FORMAT_LEFT, 10);
 }
--- Quicksearch/Quicksearch/prototype-v1/src/adminpanel.hpp
+++ Quicksearch/Quicksearch/prototype-v1/src/adminpanel.hpp
@@ -1,7 +1,13 @@
 #include "wx/string.h"
+#include "wx/listctrl.h"
 #include <wx/wx.h>
 
 class AdminPanel : public wxFrame {
 public: 
     AdminPanel(const wxString& title);
+
+private:
+    wxFont uiFont;
+    wxPanel* panel;
+    wxSizer* mainSizer;
 };
