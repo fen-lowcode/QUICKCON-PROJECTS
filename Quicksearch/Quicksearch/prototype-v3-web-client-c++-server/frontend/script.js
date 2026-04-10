2e194adbbdd7a2515d4751e88e43f7a1c47ed419
fen-lowcode <223019650+fen-lowcode@users.noreply.github.com>
2026-04-10 16:35:52 +0800

    Removed hash function from javascipt

--- Quicksearch/Quicksearch/prototype-v3-web-client-c++-server/frontend/script.js
+++ Quicksearch/Quicksearch/prototype-v3-web-client-c++-server/frontend/script.js
@@ -1,26 +1,11 @@
 
-async function hashString(message) {
-    // 1. Encode the string as a Uint8Array
-    const msgBuffer = new TextEncoder().encode(message);
-
-    // 2. Hash the message using SHA-256
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-
-    // 3. Convert ArrayBuffer to a hex string
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    
-    return hashHex;
-}
-
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault(); // Prevents page reload
 
     // Get the values from the inputs
     const firstName = document.getElementById("fname").value;
     const lastName = document.getElementById("lname").value;
-    const passBuf = document.getElementById("psw").value;
-    const password = await hashString(passBuf);
+    const password = document.getElementById("psw").value;
 
     try {
         const response = await fetch('http://127.0.0.1:2222/auth/session', {
@@ -37,7 +22,8 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
         });
 
     
-        if(response.ok){ var data = await response.json(); console.log(data)}
+        if(response.ok){ var data = await response.json(); alert(JSON.stringify(data))}
+        if(!response.ok) { var data = await response.json(); alert(JSON.stringify(data))}
         
 
     } catch (error) {
